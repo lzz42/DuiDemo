0802d7f1676ac836e168a6df47d55a2a361e16fb
lzz42 <hbuzlz@163.com>
2020-06-10 17:28:42 +0800

    init

---
+++ DuiDemo/DuiDemo.h
@@ -0,0 +1,39 @@
+﻿
+// DuiDemo.h: DuiDemo 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// CDuiDemoApp:
+// 有关此类的实现，请参阅 DuiDemo.cpp
+//
+
+class CDuiDemoApp : public CWinAppEx
+{
+public:
+	CDuiDemoApp() noexcept;
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	BOOL  m_bHiColorIcons;
+
+	virtual void PreLoadState();
+	virtual void LoadCustomState();
+	virtual void SaveCustomState();
+
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CDuiDemoApp theApp;
